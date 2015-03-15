3ab4b4466a62d646553813646406d63521fa1f2f
baberry416 <brian.a.berry@protonmail.ch>
2015-03-15 15:46:22 -0400

    Initial draft of makecachematrix and cachesolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invert <- NULL
+        
+        setmatrix <- function (y)
+                x <<- y
+                invert <<- NULL
+        getmatrix <- function () x
+        setinvert <- function (inv) invert <<- inv
+        getinvert <- function() invert
+        
+        list (setmatrix = setmatrix
+              getmatrix = getmatrix
+              setinvert = setinvert
+              getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invert <- x$getinvert ()
+        if (!is.null (invert)) {
+                message ("getting solution from cache")
+                return (invert)
+        }
+                
+        message ("inverting matrix and saving in cache" )
+        matrix <- x$getmatrix()
+        invert <- solve (matrix, ...)
+        x$setinvert (invert)
+                
+        return (invert)
 }
